ee5b4a57c62987fc3141309fbc5b3dc0c4370b2b
minhtvpy00202 <minhtvpy00202@gmail.com>
2025-11-17 09:43:39 +0700

    Lab 1

--- SOF3012_Lab1/src/main/java/Entity/User.java
+++ SOF3012_Lab1/src/main/java/Entity/User.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="Users")
 public class User {
@@ -19,54 +22,4 @@ public class User {
     String email;
     @Column(name="admin")
     boolean admin=false;
-
-    public User() {}
-
-    public User(String id, String password, String fullname, String email, boolean admin) {
-        this.id = id;
-        this.password = password;
-        this.fullname = fullname;
-        this.email = email;
-        this.admin = admin;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isAdmin() {
-        return admin;
-    }
-
-    public void setAdmin(boolean admin) {
-        this.admin = admin;
-    }
 }
